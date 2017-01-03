921bba32a82e1f278995e1fff6153b9bd43b8a2a
airfight <745756619@qq.com>
2017-01-03 15:14:04 +0800

    ///

--- SwiftJsonModel/SwiftJsonMode/Mappable.swift
+++ SwiftJsonModel/SwiftJsonMode/Mappable.swift
@@ -17,9 +17,106 @@ import Foundation
 
 public protocol BaseMappable {
     
-//    mutating func mapping(map: Map)
+    mutating func mapping(map: Map)
 }
 
-class Mappable: NSObject {
-
+public protocol Mappable: BaseMappable {
+    init?(map: Map)
 }
+
+public protocol StaticMappable: BaseMappable {
+    
+    static func objectForMapping(map: Map) -> BaseMappable?
+}
+
+public extension BaseMappable {
+    
+    public init?(JSONString: String, context: MapContext? = nil) {
+        if let obj: Self = Mapper(context: context).map(JSONString: JSONString) {
+            self = obj
+        } else {
+            return nil
+        }
+    }
+    
+    /// Initializes object from a JSON Dictionary
+    public init?(JSON: [String: Any], context: MapContext? = nil) {
+        if let obj: Self = Mapper(context: context).map(JSON: JSON) {
+            self = obj
+        } else {
+            return nil
+        }
+    }
+    
+    /// Returns the JSON Dictionary for the object
+    public func toJSON() -> [String: Any] {
+        return Mapper().toJSON(self)
+    }
+    
+    /// Returns the JSON String for the object
+    public func toJSONString(prettyPrint: Bool = false) -> String? {
+        return Mapper().toJSONString(self, prettyPrint: prettyPrint)
+    }
+}
+
+public extension Array where Element: BaseMappable {
+    
+    /// Initialize Array from a JSON String
+    public init?(JSONString: String, context: MapContext? = nil) {
+        if let obj: [Element] = Mapper(context: context).mapArray(JSONString: JSONString) {
+            self = obj
+        } else {
+            return nil
+        }
+    }
+    
+    /// Initialize Array from a JSON Array
+    public init?(JSONArray: [[String: Any]], context: MapContext? = nil) {
+        if let obj: [Element] = Mapper(context: context).mapArray(JSONArray: JSONArray) {
+            self = obj
+        } else {
+            return nil
+        }
+    }
+    
+    /// Returns the JSON Array
+    public func toJSON() -> [[String: Any]] {
+        return Mapper().toJSONArray(self)
+    }
+    
+    /// Returns the JSON String for the object
+    public func toJSONString(prettyPrint: Bool = false) -> String? {
+        return Mapper().toJSONString(self, prettyPrint: prettyPrint)
+    }
+}
+
+public extension Set where Element: BaseMappable {
+    
+    /// Initializes a set from a JSON String
+    public init?(JSONString: String, context: MapContext? = nil) {
+        if let obj: Set<Element> = Mapper(context: context).mapSet(JSONString: JSONString) {
+            self = obj
+        } else {
+            return nil
+        }
+    }
+    
+    /// Initializes a set from JSON
+    public init?(JSONArray: [[String: Any]], context: MapContext? = nil) {
+        guard let obj = Mapper(context: context).mapSet(JSONArray: JSONArray) as Set<Element>? else {
+            return nil
+        }
+        self = obj
+    }
+    
+    /// Returns the JSON Set
+    public func toJSON() -> [[String: Any]] {
+        return Mapper().toJSONSet(self)
+    }
+    
+    /// Returns the JSON String for the object
+    public func toJSONString(prettyPrint: Bool = false) -> String? {
+        return Mapper().toJSONString(self, prettyPrint: prettyPrint)
+    }
+}
+
